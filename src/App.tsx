51894834970db59e801f67322adc715d3548d0fb
gharamiananda <anandagharami.am@gmail.com>
2023-04-27 14:24:58 +0530

    demo product

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { sampleProducts } from './data/data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,18 @@ function App() {
   return (
     <>
      <header>TS Amamzona</header>
-     <main></main>
+     <main>
+      <ul>
+      {
+      sampleProducts.map(product=>(
+        <li key={product.slug}>
+          <img src={product.image} className='product-image' alt={product.name} />
+          <h2>{product.name}</h2>
+          <p>{product.price}</p>
+        </li>
+      ))}
+      </ul>
+     </main>
   <footer>All Right reserve</footer>   
     </>
   )
---
+++ src/data/data.ts
@@ -0,0 +1,54 @@
+import { Product } from "../types/Product";
+
+export const sampleProducts: Product[]=[
+    {
+        name:"Product 1",
+        slug:"string1",
+        image:"../../public/images/p1.jpg",
+
+        category:"string",
+        brand:"string",
+        price:234,
+        countInStock:23,
+        description:"string",
+        rating:3,
+        numReviews:3
+    },
+    {
+        name:"Product 2",
+        slug:"string2",
+        image:"../../public/images/p2.jpg",
+
+        category:"string",
+        brand:"string",
+        price:234,
+        countInStock:23,
+        description:"string",
+        rating:3,
+        numReviews:3
+    },
+    {
+        name:"Product3",
+        slug:"string3",
+        image:"../../public/images/p3.jpg",
+        category:"string",
+        brand:"string",
+        price:234,
+        countInStock:23,
+        description:"string",
+        rating:3,
+        numReviews:3
+    },
+    {
+        name:"Product4",
+        slug:"string4",
+        image:"../../public/images/p4.jpg",
+        category:"string",
+        brand:"string",
+        price:234,
+        countInStock:23,
+        description:"string",
+        rating:3,
+        numReviews:3
+    },
+]
