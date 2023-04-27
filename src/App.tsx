8007b23f23d35134c16e9b3d8bf6313ba225362c
gharamiananda <anandagharami.am@gmail.com>
2023-04-27 13:26:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <header>TS Amamzona</header>
+     <main></main>
+  <footer>All Right reserve</footer>   
+    </>
+  )
+}
+
+export default App
